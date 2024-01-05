0054016eb0c4a6b94e022a8496b59143903bad86
k-clavijo <kclavijo@docokids.com>
2024-01-04 21:45:01 -0500

    step-2

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -34,59 +34,56 @@ class _LoginViewState extends State<LoginView> {
       appBar: AppBar(
         title: const Text('Login'),
       ),
-      body: FutureBuilder(
-        future: Firebase.initializeApp(
-          options: DefaultFirebaseOptions.currentPlatform
-        ),
-        builder: (context, snapshot) {
-          switch (snapshot.connectionState) {         
-            case ConnectionState.done:
-              return Column(
-                children: [
-                  TextField(
-                    controller: _email,
-                    enableSuggestions: false,
-                    autocorrect: false,
-                    keyboardType: TextInputType.emailAddress,
-                    decoration: InputDecoration(
-                      hintText: 'Enter your email here'
-                    ),
-                  ),
-                  TextField(
-                    controller: _password,
-                    obscureText: true,
-                    enableSuggestions: false,
-                    autocorrect: false,
-                    decoration: InputDecoration(
-                      hintText: 'Enter your password here'
-                    ),
-                  ),
-                  TextButton(
-                    onPressed: () async {
-                      final email = _email.text;
-                      final password = _password.text;
-                      try {
-                        final userCredential = await FirebaseAuth.instance.signInWithEmailAndPassword(
-                        email: email,
-                        password: password
-                      );
-                      
-                      print(userCredential);
-                      } on FirebaseAuthException catch (e) {
-                        print(e.code);
-                        if (e.code == 'invalid-credential') {
-                          print('User not found or invalid credentials');
-                        }
-                      }                     
-                    },
-                    child: const Text('Login')
-                  ),
-                ],
+      body: Column(
+        children: [
+          TextField(
+            controller: _email,
+            enableSuggestions: false,
+            autocorrect: false,
+            keyboardType: TextInputType.emailAddress,
+            decoration: InputDecoration(
+              hintText: 'Enter your email here'
+            ),
+          ),
+          TextField(
+            controller: _password,
+            obscureText: true,
+            enableSuggestions: false,
+            autocorrect: false,
+            decoration: InputDecoration(
+              hintText: 'Enter your password here'
+            ),
+          ),
+          TextButton(
+            onPressed: () async {
+              final email = _email.text;
+              final password = _password.text;
+              try {
+                final userCredential = await FirebaseAuth.instance.signInWithEmailAndPassword(
+                email: email,
+                password: password
               );
-            default:
-              return const Text('Loading...');
-          }
-        },
+              
+              print(userCredential);
+              } on FirebaseAuthException catch (e) {
+                print(e.code);
+                if (e.code == 'invalid-credential') {
+                  print('User not found or invalid credentials');
+                }
+              }                     
+            },
+            child: const Text('Login')
+          ),
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).pushNamedAndRemoveUntil(
+                '/register/',
+                (route) => false
+              );
+            },
+            child: const Text('Not registered yet? Register here!')
+          )
+        ],
       ),
     );
   }
